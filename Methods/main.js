246538684cb6c6ef361800060a94ee3241604e88
Prathamesh More <moreprathameshmore@hotmail.com>
2018-06-06 12:07:34 +0530

    Add Method Practise

---
+++ Methods/main.js
@@ -0,0 +1,12 @@
+var obj = {
+    name: "Prathamesh",
+    age: 45,
+    isSingle: true,
+    friends: ["Santosh", "Nawaz", "Shubham"],
+    add: function (x, y) {
+        return x + y;
+    }
+}
+
+console.log(obj.add);
+
